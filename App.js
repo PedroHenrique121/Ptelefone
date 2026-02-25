f0961fcde61a37ad30dabbc468019f9211c70489
PedroHenrique121 <pedrohenriquelimadasilva97@gmail.com>
2026-02-25 10:18:26 -0300

    aula sobre chamadas telefonicas

--- App.js
+++ App.js
@@ -59,7 +59,7 @@ return (
 <Text>Cadastro</Text>
 <Text>Nome</Text>
 <input type="text" placeholder="Username" />
-<Text>CPF</Text>
+<Text>CPf</Text>
 <input type="password" placeholder="CPF" />
 <Text>Email</Text>
 <input type="text" placeholder="E-mail" />
